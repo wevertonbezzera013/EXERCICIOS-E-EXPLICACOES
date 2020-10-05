ab9a792ff29c557777ad32379919554a91372376
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-05 12:06:06 -0300

    Create exercicio01.py

---
+++ PYTHON 3/EXERCÍCIOS/exercicio01.py
@@ -0,0 +1,11 @@
+#Após a leitura sobre a primeira aula de Python, seu desafio de hoje será fazer o programa mostrar as seguintes frases:
+
+#1) Olá, Mundo!
+#2) Olá, meu nome é {seu nome}!
+#3) spam eggs!
+#4) spameggs!
+#5) spameggsspameggsspameggs !
+#6) 5 + 5
+#7) 8 / 2
+
+#Bons estudos, Devs! ;)
