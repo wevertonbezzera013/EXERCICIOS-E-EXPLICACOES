20007066a085c8f70967cf4c7e454f679a8bdd86
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-05 12:06:18 -0300

    Create Aula01.py

---
+++ PYTHON 3/EXPLICAÇÕES/Aula01.py
@@ -0,0 +1,17 @@
+#Nessa aula iremos aprender sobre a função print do Python
+#Nas linguagens de programação nós desenvolvedores precisamos visualizar
+#nossos códigos. Mas como seria possível mostrar na tela uma frase como 'Olá, mundo!' utilizando códigos em Python?
+#Python é uma linguagem bem simples de se entender. Assim como a maioria das linguagens de programação, os códigos em Python
+#são escritos todos em inglês. Se você deseja mostrar uma frase na tela, então você deseja utilizar a função 'imprimir', que em
+#inglês se escreve 'print'. Porém, é necessário que sejam seguidas as estruturas dessa linguagem para que o código funcione.
+#Por exemplo, sempre que você deseja ver a sua menssagem na tela, é necessário que você siga essa estrutura:
+print('Olá, munndo!')
+
+#Mas cuidado, muita atenção às aspas, elas são muito importantes para qualquer linguagem de programação! As aspas simples ('') ou
+#duplas ("") são usadas para dizer ao programa que aqueles item que está dentro do programa se trata de uma palavra ou, como é
+#de costume falar nas linguagens de programação, se trata de uma string!
+
+#Esta foi a primeira aula de Python e também a primeira aula do nosso projeto para ajudar a nossa comunidade de brasileiros que desejam
+#estudar, exercitar, compartilhar e aprender programação. Todos são muitíssimos bem-vindos a nossa comunidade!
+#Leiam os textos, façam os exercícios, que todos serão capazes de evoluir muito!
+#Agora que a aula foi finalizada, acesse a pasta de exercícios! ;)
