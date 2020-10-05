c46f91dbaeba971e61a5e8340d78c2f77634f3a9
wevertonbezzera013 <wevertonbezzera013@gmail.com>
2020-10-05 12:06:10 -0300

    Create gabarito01.py

---
+++ PYTHON 3/EXERCÍCIOS/gabarito01.py
@@ -0,0 +1,25 @@
+#1) Olá, Mundo!
+print('Olá, Mundo!')
+
+#2) Olá, meu nome é {seu nome}!
+nome = str(input('Digite seu nome: '))
+print(f'Olá, meu nome é {nome}!')
+
+#3) spam eggs!
+print('spam eggs!')
+
+#4) spameggs!
+print('spam' + 'eggs!')
+
+#5) spameggsspameggsspameggs !
+print('spameggs' *3, '!')
+
+#6) 5 + 5
+print(5 + 5)
+
+#7) 8 / 2
+print(8 / 2)
+
+#Parabéns,Dev! Você concluiu a primeira aula!
+#Se a sua solução não está parecida com a minha, não tem problema algum!
+#Esta é só a primeira aula!
